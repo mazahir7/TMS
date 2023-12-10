9c299b7aa7416c95b7632bcfe0f7c407cbdfa82f
Mazahir Attari <91468649+mazahir7@users.noreply.github.com>
2023-12-10 20:00:42 +0530

    Add files via upload

--- assests/javascript/script.js
+++ assests/javascript/script.js
@@ -4,10 +4,39 @@
 const phoneMenu = document.querySelector(".menu-nav");
 const navbtnContainer = document.querySelector(".nav-btn-container");
 const navBtn = document.querySelector(".nav-btn");
+const headerSection = document.querySelector(".header");
+const sectionHero = document.querySelector(".section-hero");
 
 // hero section
 const imageSlider = document.querySelector(".slider-image");
 let sliderImageNumber = 1;
+
+// hire-us section
+const formLabels = document.querySelectorAll("label");
+
+// testimonials section
+const stars1 = document.querySelectorAll(".rating-1 i");
+const stars2 = document.querySelectorAll(".rating-2 i");
+const stars3 = document.querySelectorAll(".rating-3 i");
+
+const stickyNavbar = function () {
+  const clientY = scrollY;
+  console.log("ss");
+  if (clientY > 500) {
+    headerSection.classList.add("sticky");
+    sectionHero.style.paddingTop = `9rem`;
+  }
+  else {
+    headerSection.classList.remove("sticky");
+    sectionHero.style.paddingTop = `2rem`;
+  }
+}
+
+document.addEventListener('scroll', stickyNavbar);
+
+
+
+
 
 // navigation open and close
 navbtnContainer.addEventListener("click", () => {
@@ -28,3 +57,38 @@ function slideImage() {
   }, 2400);
 }
 slideImage();
+
+
+
+// wave animation in form inputs
+
+formLabels.forEach(labelEl => {
+  const labeltext = labelEl.textContent.split("");
+
+  labelEl.innerHTML = "";
+
+  labeltext.forEach((letter, i) => {
+    const spanEl = document.createElement("span");
+    spanEl.innerText = letter;
+
+    if (spanEl.innerText !== " ") {
+      spanEl.style.transitionDelay = `${40 * i}ms`;
+    }
+    labelEl.appendChild(spanEl);
+  });
+
+});
+
+// wave animation in testimonials section for stars
+
+const starWaveAnimation = function (stars) {
+  stars.forEach((star, i) => {
+    star.style.animationDelay = `${150 * i}ms`;
+  });
+}
+
+starWaveAnimation(stars1);
+starWaveAnimation(stars2);
+starWaveAnimation(stars3);
+
+
